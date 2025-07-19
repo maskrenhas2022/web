99463f3f86fabfcbd6b5236857723a45f53ae73c
maskrenhas2022 <maskrenhas02@gmail.com>
2025-07-19 17:57:57 -0400

    updates

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { CreateRoom } from './pages/create-room';
 import { Room } from "./pages/room";
 
+
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Rota para a página inicial (CreateRoom) */}
-        <Route index element={<CreateRoom />} />
 
-        {/* Rota para a página Room, acessível via "/room" */}
-        <Route path="/room" element={<Room />} />
+    <QueryClientProvider client={queryClient}>
+
+
+ <BrowserRouter>
+      <Routes>
+    
+        <Route element={<CreateRoom />} index/>
+
+       
+        <Route element = {<Room />} path="/room" />
       </Routes>
     </BrowserRouter>
-  );
+  
+
+    </QueryClientProvider>
+     );
 }
--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,10 +1,45 @@
+import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
 
+type  GetRoomsAPIResponse = Array< {
+
+  id : string 
+  name: string
+}>
+
+
+
+
 export function CreateRoom(){
+
+    const { data, isLoading} = useQuery({
+
+        queryKey: ['get-rooms'],
+
+        queryFn: async () => {
+
+        const response = await fetch('http://localhost:3333/rooms')
+
+        const result : GetRoomsAPIResponse = await response.json()
+
+        return result
+        
+        },
+    })
     
       return <div>
  
      <div>Create Room</div>
+
+
+      {isLoading && <p>Carregando... </p>  }
+
+       <div>        { data &&  data.map(room => {
+
+            return <p key={room.id}>{room.name}</p>
+       })}         </div>     
+
+    
     
       <Link className="underline" to="/room">Acessar Sala </Link>
           
